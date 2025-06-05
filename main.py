72182eb79c871079df558d8f3953f8ffaa8788b8
officeofmerkinmuffley <officeofmerkinmuffley@proton.me>
2025-06-05 16:32:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CRYPTO_SYMBOLS = ["BTC-USD"]
 STOCK_SYMBOLS = [
     "GME", "BITO", "MP", "UUUU", "REMX", "IBIT", "HUT", "MARA", "MSTR",
     "DJT", "NXTT", "YGME", "YBITO", "MSTY", "GRYP", "GITS", "BMGL",
-    "TSLA", "PLTR", "COIN", "HOOD","VIXW","SOFI","GDX","VYM","BWEB","WOLF","MARA","RIOT","SMLR","STRK","XYZ",
+    "TSLA", "PLTR", "COIN", "HOOD","VIXX","SOFI","GDX","VYM","BWEB","WOLF","MARA","RIOT","SMLR","STRK","XYZ",
 ]
 
 # Polygon endpoints
